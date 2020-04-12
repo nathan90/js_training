f5e7898061c1d9cf7f7afcad94221c1e5bb26a28
nathan90 <46viswanath@gmail.com>
2020-04-13 02:00:56 +0530

    add sum of N numbers program

---
+++ exercises/008_sum_n_numbers.js
@@ -0,0 +1,14 @@
+//8. Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
+const readlineSync = require("readline-sync");
+const num = readlineSync.question("Enter a Positive Integer:");
+
+const sumOfNnumbers = (num) => {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    console.log("Sum of numbers from 1 to ", num, ":", sum);
+}
+
+sumOfNnumbers(num);
