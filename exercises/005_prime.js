50305604381cd3beccba4c4788a49436a1c6271e
nathan90 <46viswanath@gmail.com>
2020-04-13 01:45:51 +0530

    add prime number code

---
+++ exercises/005_prime.js
@@ -0,0 +1,19 @@
+//Enter a number and find if it's prime
+const readlineSync = require("readline-sync");
+const num = readlineSync.question("Enter a Positive Integer:");
+
+const isPrime = (num) => {
+    if (num <= 1) {
+        return "Number is not prime";
+    } else {
+        for (let i = 2; i < Math.floor(num/2)+1; i++) {
+            if (num%i === 0) {
+                return "Number is not prime";
+            } else {
+                return "Number is prime";
+            }
+        }
+    }
+}
+
+console.log(isPrime(num));
