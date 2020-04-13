2badd9227e572504d08e5a600804fce7839ca30e
nathan90 <46viswanath@gmail.com>
2020-04-13 16:20:50 +0530

    Addressed review comments

--- exercises/009_mutlipleOf3or5sum.js
+++ exercises/009_mutlipleOf3or5sum.js
@@ -7,7 +7,7 @@ const num = readlineSync.question("Enter a Positive Integer:");
 const sumOfNnumbers = (num) => {
     let sum = 0;
     for (let i = 1; i <= num; i++) {
-        if (i%3 ===0 || i%5 === 0) {
+        if (i%3 === 0 || i%5 === 0) {
             sum += i;
         }
     }
