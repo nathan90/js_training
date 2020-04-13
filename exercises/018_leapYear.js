852a33f5d0711fc8bfce3f9e02eeb03f9575a8f7
nathan90 <46viswanath@gmail.com>
2020-04-14 04:15:12 +0530

    Add leap year code

---
+++ exercises/018_leapYear.js
@@ -0,0 +1,35 @@
+//Write a program that prints the next 20 leap years.
+const readlineSync = require("readline-sync");
+const year = parseInt(readlineSync.question("Enter a Starting Year: "));
+
+const leapYear = (year) => {
+    rem = year % 4
+    if (rem === 0) {
+        for (let i = 1; i <= 20; i++) {
+            console.log(year)
+            year += 4
+        }
+    } else {
+        if (rem === 1) {
+            year += 3
+            for (let i = 1; i <= 20; i++) {
+                console.log(year)
+                year += 4
+            } 
+        } else if (rem === 2) {
+            year += 2
+            for (let i = 1; i <= 20; i++) {
+                console.log(year)
+                year += 4
+            }
+        } else {
+            year += 1;
+            for (let i = 1; i <= 20; i++) {
+                console.log(year)
+                year += 4
+            }
+        }
+    }
+}
+
+leapYear(year)
