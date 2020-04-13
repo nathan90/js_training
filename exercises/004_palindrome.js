6ca61751ad2a3f1852e4b834aa8b29e1f940ba32
nathan90 <46viswanath@gmail.com>
2020-04-13 15:47:54 +0530

    addressed review comments

--- exercises/004_palindrome.js
+++ exercises/004_palindrome.js
@@ -1,18 +1,18 @@
 //4. Enter a  string and find if it's a palindrome
 const readlineSync = require("readline-sync");
-const string = readlineSync.question("Enter a String:")
+const string = readlineSync.question("Enter a String:");
 
 const palindrome = (string) => {
     let len = string.length;
-    str = string.toLowerCase();
+    let str = string.toLowerCase();
 
-    for (let i = 0; i< Math.floor(len/2); i++) {
+    for (let i = 0; i < Math.floor(len/2); i++) {
         if (str[i] !== str[len -1 - i]) {
-            return "String is not Palindrome"
+            return "String is not Palindrome";
         } else {
-            return "String is Palindrome"
+            return "String is Palindrome";
         }
     }
 }
 
-console.log(palindrome(string))
+console.log(palindrome(string));
