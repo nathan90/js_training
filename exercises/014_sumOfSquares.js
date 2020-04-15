bafc92b94ae4bd9253d4853d85aaeb94bea09125
nathan90 <46viswanath@gmail.com>
2020-04-15 15:21:18 +0530

    added return, addressed comments

--- exercises/014_sumOfSquares.js
+++ exercises/014_sumOfSquares.js
@@ -3,20 +3,24 @@
 const readlineSync = require("readline-sync");
 const num = parseInt(readlineSync.question("Enter a Number: "));
 
-function sumOfSquares(num) {
-    for (x = 1; x < num; x++) {
+const sumOfSquares = (num) => {
+    for ( let x = 1; x < num; x++) {
         if (x * x < num) {
-            for (y = 1; y * y < num; y++) {
+            for (let y = 1; y * y < num; y++) {
                 if ((x * x + y * y) === num) {
-                    console.log(num + " can be expressed as square of " + x + " and " + y);
-                    return x
+                    return [x, y]
                 }
             }
         } else if (x * x >= num) {
-            console.log(num, " cannot be expressed as the sum of squares of two whole numbers");
             return 
         }
     }
 }
 
-sumOfSquares(num)
+//calling the function
+let wholenumbers = sumOfSquares(num)
+if (wholenumbers != undefined) {
+    console.log(num + " can be expressed as square of " + wholenumbers[0] + " and " + wholenumbers[1]);
+} else {
+    console.log(num, "cannot be expressed as the sum of squares of two whole numbers");
+}
