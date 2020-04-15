6997351abdd2e9cd63a4b1a873e80d8ccf94607b
nathan90 <46viswanath@gmail.com>
2020-04-15 16:05:04 +0530

    add positive or negative program

---
+++ exercises/023_positveOrNegative.js
@@ -0,0 +1,13 @@
+//Write a program to check a number is positive or negative
+
+const posOrNeg = (num) => {
+    if (num < 0) {
+        return "Number is negative"
+    } else {
+        return "Number is positive"
+    }
+}
+
+//Calling the function
+
+console.log(posOrNeg(-10))
