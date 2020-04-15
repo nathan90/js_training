73757cd307bff36ac582a54bb9b42b00ca98f84f
nathan90 <46viswanath@gmail.com>
2020-04-15 15:45:48 +0530

    added upper lower special find program

---
+++ exercises/022_upperLowerSpecial.js
@@ -0,0 +1,13 @@
+// Write a program to check Uppercase, Lowercase or special character
+const isAlphabet = (character) => {
+    if (character >= 'a' && character <= 'z') {
+        return "Character is lower case"
+    } else if (character >= 'A' && character <= 'Z'){
+        return "Character is upper case";   
+    }   else {
+        return "Character is special character"
+    }
+}
+
+//calling the function
+console.log(isAlphabet('-'))
