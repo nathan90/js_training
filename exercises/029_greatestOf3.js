e2ec90aedc8c9962d6316d40f3467b6392582e73
nathan90 <46viswanath@gmail.com>
2020-04-15 16:56:18 +0530

    added greater than for 3 numbers code

---
+++ exercises/029_greatestOf3.js
@@ -0,0 +1,14 @@
+//Greatest of three numbers
+
+const greatest = (num1, num2, num3) => {
+    if ((num1 > num2) && (num1 > num3)) {
+        return num1;
+    } else if (num2 > num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+//Calling the function
+console.log("Greatest number is ", greatest(40, 120, 30))
