1136271961fba82925ab51afc97113b3486a7d07
nathan90 <46viswanath@gmail.com>
2020-04-17 02:27:55 +0530

    minor format correction

--- exercises/040_perfectNumber.js
+++ exercises/040_perfectNumber.js
@@ -4,17 +4,17 @@ const perfectNumber = (number) => {
     let sum = 0;
     for (let i = 1; i <= Math.floor(number / 2); i++) {
         if (number % i === 0) {
-            sum += i
+            sum += i;
         }
     }
     if (sum === number) {
-        return "Perfect Number"
+        return "Perfect Number";
     }
     else {
-        return "Not a Perfect Number"
+        return "Not a Perfect Number";
     }
 }
 
 //Enter the number
-let number = 6
-console.log(`Number ${number} is a`, perfectNumber(number))
+let number = 6;
+console.log(`Number ${number} is a`, perfectNumber(number));
