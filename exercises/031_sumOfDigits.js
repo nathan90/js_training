f63eae02f4044ec53e649cb1f5f9b85594f13a87
nathan90 <46viswanath@gmail.com>
2020-04-17 03:52:26 +0530

    format correction

--- exercises/031_sumOfDigits.js
+++ exercises/031_sumOfDigits.js
@@ -1,14 +1,14 @@
 //31. Sum of digits of a number
 
 const sumOfDigits = (number) => {
-    let sum = 0
-    while (number != 0) {
-        sum += number % 10
-        number = Math.floor(number / 10)    
+    let sum = 0;
+    while (number !== 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);  
     }
     return sum;
 }
 
 //calling the function
 
-console.log("The sum of digits is: ", sumOfDigits(139))
+console.log("The sum of digits is: ", sumOfDigits(139));
