86b7ed81362bd39661874d036adf65843040d726
nathan90 <46viswanath@gmail.com>
2020-04-17 02:35:46 +0530

    minor format changes

--- exercises/042_factorsOfNumber.js
+++ exercises/042_factorsOfNumber.js
@@ -1,15 +1,15 @@
 //42. Find all Factors of a number
 
 const factors = (number) => {
-    factorsArray = [] 
+    factorsArray = [];
     for (i = 1; i <= Math.floor(number / 2); i++) {
         if (number % i === 0) {
-            factorsArray.push(i)
+            factorsArray.push(i);
         }
     }
-    return factorsArray
+    return factorsArray;
 }
 
 //Calling the function
-let number = 6
-console.log(`The factors of the number ${number}:=>`, factors(number))
+let number = 6;
+console.log(`The factors of the number ${number}:=>`, factors(number));
