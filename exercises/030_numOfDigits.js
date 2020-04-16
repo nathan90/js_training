7cd9f41c2a2293eff21cd0c063400cf465a2c9fd
nathan90 <46viswanath@gmail.com>
2020-04-17 03:49:43 +0530

    minor format changes

--- exercises/030_numOfDigits.js
+++ exercises/030_numOfDigits.js
@@ -1,13 +1,13 @@
 //Number of digits in an integer
 
 const numberOfDigits = (number) => {
-    let count = 0
+    let count = 0;
     while (number != 0) {
-        number = Math.floor(number / 10)
-        count++
+        number = Math.floor(number / 10);
+        count++;
     }
     return count;
 }
 
 //Calling the function
-console.log("Number of digits are ", numberOfDigits(2133))
+console.log("Number of digits are ", numberOfDigits(2133));
