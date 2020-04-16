c91d6edd99336ab8fa5ea054c6f54af895e3c811
nathan90 <46viswanath@gmail.com>
2020-04-17 03:31:29 +0530

    minor format changes

--- exercises/019_vowelOrConsonant.js
+++ exercises/019_vowelOrConsonant.js
@@ -3,17 +3,17 @@ const readlineSync = require("readline-sync");
 const string = readlineSync.question("Enter a String: ");
 
 const vowelOrConsonent = (string) => {
-    let stringArray = string.split("")
-    let arrayLen = stringArray.length
-    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let stringArray = string.split("");
+    let arrayLen = stringArray.length;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
     
     for (let i = 0; i < arrayLen; i++) {
         if (vowels.includes(stringArray[i])) {
             console.log("Character " + stringArray[i] + " is a vowel");
         } else {
-            console.log("Character " + stringArray[i] + " is a consonant")
+            console.log("Character " + stringArray[i] + " is a consonant");
         }
     }
 }
 
-vowelOrConsonent(string)
+vowelOrConsonent(string);
