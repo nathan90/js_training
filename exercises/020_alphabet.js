4c54927accd16e15136874a1730625a4b8d1cdcd
nathan90 <46viswanath@gmail.com>
2020-04-17 03:33:34 +0530

    minor format additions

--- exercises/020_alphabet.js
+++ exercises/020_alphabet.js
@@ -2,19 +2,19 @@
 
 const isAlphabet = (character) => {
     if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
 //Enter the value of character
-let character = '9'
+let character = '9';
 
-let ifCharacter = isAlphabet(character)
+let ifCharacter = isAlphabet(character);
 
 if (ifCharacter === true) {
-    console.log("Character is an alphabet")
+    console.log("Character is an alphabet");
 } else {
-    console.log("Character is not an alphabet")
+    console.log("Character is not an alphabet");
 }
