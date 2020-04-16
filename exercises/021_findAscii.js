256b10c23d36d4b56f64b1bf1644f6f46a2a05f6
nathan90 <46viswanath@gmail.com>
2020-04-17 03:35:03 +0530

    minor format corrections

--- exercises/021_findAscii.js
+++ exercises/021_findAscii.js
@@ -1,10 +1,10 @@
 //21. Write a program to check Ascii values of a character
 
 const findAscii = (character) => { 
-    let asciivalue = character.charCodeAt()
-    return asciivalue
+    let asciivalue = character.charCodeAt();
+    return asciivalue;
 }
 
 // Calling the function
 
-console.log("Ascii value is : ", findAscii('a'))
+console.log("Ascii value is : ", findAscii('a'));
