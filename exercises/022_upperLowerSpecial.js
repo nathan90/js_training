cd35e055b7846fe0ebb5e818fc31942eb4f4a831
nathan90 <46viswanath@gmail.com>
2020-04-17 03:37:25 +0530

    minor format changes

--- exercises/022_upperLowerSpecial.js
+++ exercises/022_upperLowerSpecial.js
@@ -1,13 +1,14 @@
 // Write a program to check Uppercase, Lowercase or special character
+
 const isAlphabet = (character) => {
     if (character >= 'a' && character <= 'z') {
-        return "Character is lower case"
+        return "Character is lower case";
     } else if (character >= 'A' && character <= 'Z'){
         return "Character is upper case";   
     }   else {
-        return "Character is special character"
+        return "Character is special character";
     }
 }
 
 //calling the function
-console.log(isAlphabet('-'))
+console.log(isAlphabet('-'));
