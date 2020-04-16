9f51d8019a9359dedfb5d4825ab10b4c1ba283ad
nathan90 <46viswanath@gmail.com>
2020-04-17 01:56:05 +0530

    added power of Number program

---
+++ exercises/041_powerOfNumber.js
@@ -0,0 +1,10 @@
+//Find the power of a number
+const powerOf = (number, power) => {
+    return Math.pow(number, power)
+}
+
+//calling the function
+let number = 10;
+let power = 3;
+
+console.log(powerOf(number, power));
