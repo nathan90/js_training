a056cd835d3625e6d3686fe1ba4aa9e037186aff
nathan90 <46viswanath@gmail.com>
2020-04-17 12:40:25 +0530

    added prime number upto n program

---
+++ exercises/016_allPrimeNumbers.js
@@ -0,0 +1,28 @@
+//Write a program that prints all prime numbers. 
+//(Note: if your programming language does not support arbitrary size numbers, 
+//printing all primes up to the largest number you can easily represent is fine too.)
+
+allPrimeNumbers = (number) => {
+    if (number <= 1) {
+        return "Enter a number greater than 1";
+    }
+    let primeNumbers = [2];
+    if (number === 2) {
+        return primeNumbers;
+    }
+    for (i = 3; i <= number; i = i+2) {
+        let flagPrime = true;
+        for (j = 3; j < Math.floor(i/2) + 1; j = j + 2) {
+            if (i % j === 0) {
+                flagPrime = false;
+                break;
+            }
+        }
+        if (flagPrime === true) {
+            primeNumbers.push(i);
+        }
+    }
+    return primeNumbers;
+}
+
+console.log(allPrimeNumbers(100));
