5ed6df78c5e07f602ed915485a6f98507b12b912
nathan90 <46viswanath@gmail.com>
2020-04-19 04:50:53 +0530

    added largest in list program

---
+++ exercises/063_LargestInList.js
@@ -0,0 +1,10 @@
+//Write a function that returns the largest element in a list.
+
+const maxInList = (arr) => {
+    return Math.max.apply(Math, arr);
+}
+
+//calling the function
+let arr = [1,3,34,2,8];
+console.log(`The maximum value in the list [${arr}] is`, maxInList(arr));
+
