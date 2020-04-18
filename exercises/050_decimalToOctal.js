bca7ac65fe3b75a2f623bd1f63493a2f5efe14a7
nathan90 <46viswanath@gmail.com>
2020-04-19 03:16:58 +0530

    added decimal to octal code

---
+++ exercises/050_decimalToOctal.js
@@ -0,0 +1,9 @@
+//49. Decimal to octal conversion
+
+decimalToOctal = (num) => {
+    let octal = parseInt(num, 8);
+    return octal;
+}
+
+// The decimal number is 10 and corresponding Octal is 8
+console.log(decimalToOctal(10));
