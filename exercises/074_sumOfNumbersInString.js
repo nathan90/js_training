b10387e0a02a402795f39bfe66e13c89857c39ff
nathan90 <46viswanath@gmail.com>
2020-05-07 04:50:52 +0530

    add sum of numbers in a string program

---
+++ exercises/074_sumOfNumbersInString.js
@@ -0,0 +1,18 @@
+// Sum of numbers in a string
+
+sumOfDigits = (number) => {
+    let sum = 0;
+    while (number !== 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);  
+    }
+    return sum;
+}
+
+sumOfNumberString = (string) => {
+    let number = parseInt('abc12345 ssds06789'.replace(/[^0-9\.]/g, ''), 10);
+    let sum = sumOfDigits(number);
+    return sum;
+}
+
+console.log(sumOfNumberString('abc12345 ssds6789'));
